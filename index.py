a2b4eb56e03e74319148fadcb01f521a954601f1
Alperen <67062896+xtandeasdev@users.noreply.github.com>
2023-12-18 12:55:17 +0300

    Update index.py

--- index.py
+++ index.py
@@ -17,7 +17,7 @@ if check_connection() == False:
 for dir in dirs:
     update(dir)
 
-if sys.argv[1] != None:
+if len(sys.argv) >= 2:
     file = os.path.isfile(sys.argv[1]+".py")
     if file:
         exec(open(sys.argv[1]+".py", "r", encoding="utf-8").read())
@@ -25,4 +25,3 @@ if sys.argv[1] != None:
         print("Böyle bir sistem bulunmamakta hangisi kullanmak istemiştiniz?\n"+systems)
 else:
     print("Bir sistem seçmek zorundasınız örn. python index.py indexing\nSistemler: "+systems)
-    

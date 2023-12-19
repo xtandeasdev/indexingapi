2434bba8a0d53c445fcae9a6c88f679c1be603be
Alperen <67062896+xtandeasdev@users.noreply.github.com>
2023-12-19 12:20:35 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import argparse
 import requests
-from updater import update
+
 import sys
 import os
 systems = "Indexing\nFinder"
-dirs = os.listdir()
+
 def check_connection():
     try: 
         r = requests.get("https://www.google.com",timeout=10)
@@ -14,13 +14,6 @@ def check_connection():
 if check_connection() == False:
     print("Bu programı kullanabilmek için internet bağlantısına ihtiyacınız var.")
     exit()
-check_updates = input("Güncellemeleri denetlemek istiyor musunuz? (E/H): (Varsayılan: Denetleme) ")
-if check_updates.lower() == "y" or check_updates.lower() == "e" or check_updates.lower() == "yes" or check_updates.lower() == "evet":
-    for dir in dirs:
-        update(dir)
-elif check_updates.lower() == "n" or check_updates.lower() == "h" or check_updates.lower() == "no" or check_updates.lower() == "hayır":
-    print("Güncellemeler denetlenmiyor.")
-    
 if len(sys.argv) >= 2:
     file = os.path.isfile(sys.argv[1]+".py")
     if file:

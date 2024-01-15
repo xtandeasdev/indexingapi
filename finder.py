72d63d6fc4384da9da40bca1e220b0ab17e2c602
Alperen <67062896+xtandeasdev@users.noreply.github.com>
2024-01-16 00:22:17 +0300

    Update finder.py

--- finder.py
+++ finder.py
@@ -3,7 +3,7 @@ import json
 from bs4 import BeautifulSoup
 from datetime import datetime
 from colorama import Fore, Back, Style, init
-init(autoReset=True)
+init(autoreset=True)
 headers = {
     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
 }
